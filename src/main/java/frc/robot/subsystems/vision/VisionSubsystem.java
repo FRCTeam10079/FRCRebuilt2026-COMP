c8aebac89e3afc625016fdf2c64a3e04549efdf8
risithcha <risithcha@gmail.com>
2026-03-14 13:38:28 -0700

    loggy loggy log. I am a log. Wood log? Who knows. Logsssssssss

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.LimelightHelpers;
-import frc.robot.statemachine.RobotStateMachine;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 import org.littletonrobotics.junction.Logger;
 
@@ -49,11 +48,11 @@ public class VisionSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    //boolean isAuto = RobotStateMachine.getInstance().getMatchState().autonomous;
-    //Logger.recordOutput("Vision/AutoSkipped", isAuto);
-    //if (isAuto) {
+    // boolean isAuto = RobotStateMachine.getInstance().getMatchState().autonomous;
+    // Logger.recordOutput("Vision/AutoSkipped", isAuto);
+    // if (isAuto) {
     //  return;
-    //}
+    // }
 
     String[] names = VisionConstants.LIMELIGHT_NAMES;
     Pose2d odoPose = drivetrain.getState().Pose;

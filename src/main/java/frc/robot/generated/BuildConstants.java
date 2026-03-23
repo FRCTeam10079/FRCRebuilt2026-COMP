25a66ee2cc185151b66a385dec041a3845d2ab77
ArrowDynamics10079 <roboticsbothell@gmail.com>
2026-03-23 16:56:29 -0700

    Changed buildConstants to go into Generated rather than src

---
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.generated;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRCRebuilt2026-COMP";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "ab4bcedd091b8566674e5383f7668b090cd70702";
+  public static final String GIT_DATE = "2026-03-23 16:53:13 PDT";
+  public static final String GIT_BRANCH = "feature/git-logging";
+  public static final String BUILD_DATE = "2026-03-23 16:53:14 PDT";
+  public static final long BUILD_UNIX_TIME = 1774309994029L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}

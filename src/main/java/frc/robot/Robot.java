3f027937fa777e7f3e6b9a335e087fe4423cc1f7
ArrowDynamics10079 <roboticsbothell@gmail.com>
2026-03-23 16:58:14 -0700

    Author: Dhruv | Updated build constants to now exist in generated subfolder

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.generated.BuildConstants;
 import frc.robot.lib.LaunchCalculator;
 import frc.robot.lib.PowerDiagnosticsLogger;
 import frc.robot.lib.ShooterMath;

076888dcb0d4b9c36a048ca326b9de4c9167198c
risithcha <risithcha@gmail.com>
2026-03-25 00:54:54 -0700

    Very very very simple change to ensure indexer doesn't run while intaking.

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -232,7 +232,8 @@ public class Superstructure extends SubsystemBase {
   private void applyCollect() {
     pivot.setWantedState(PivotSubsystem.WantedState.DEPLOY);
     intake.setWantedState(IntakeWheelsSubsystem.WantedState.INTAKE);
-    indexer.setWantedState(IndexerSubsystem.WantedState.INDEX);
+    indexer.setWantedState(
+        IndexerSubsystem.WantedState.OFF); // Indexer only runs during shooting/feed, never intake.
     shooter.setWantedState(ShooterSubsystem.WantedState.OFF);
     trackPivotContinuously();
   }
